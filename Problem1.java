61ad888e6979fa1fff8b0d21a908b443e9a90e47
Murthy <murthycvln@gmail.com>
2025-12-28 18:16:02 -0800

    Add missing number algorithm in Problem1.java
    Implement binary search to find the missing number in a sorted array of natural numbers.

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,33 @@
+//Missing Number in Sorted Array of Natural Numbers
+//https://www.geeksforgeeks.org/problems/missing-number-in-sorted-array-of-natural-numbers/1
 
+//Time: O(log n)
+//Space: O(1)
+
+class Solution {
+    int missingNum(int arr[]) {
+        
+        //[1,2,3,5]
+        
+        // code here
+        int low=0;
+        int high = arr.length-1;
+        
+        while(low <= high) {
+            int mid = low + (high-low) /2;
+            
+            if(arr[mid] == mid+1) {
+                //missing element is in the right half
+                //move low to mid+1
+                low = mid+1;
+            }
+            else {
+                // missing element in the left half
+                // move high to mid-1
+                high = mid-1;
+            }
+        }
+        
+        return low+1;
+    }
+}
